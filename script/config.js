1158decc2cc0b50963d9fd7a2f0930f7fa3fb356
Hopeful117 <135059935+Hopeful117@users.noreply.github.com>
2024-09-03 19:17:58 +0200

    Add files via upload

---
+++ script/config.js
@@ -0,0 +1,44 @@
+
+const tableauMotsEN1 = ["Hello","Yes","No","Please","Thank you","Good Bye","Sorry","Water","Food","Help"];
+const tableauMotsTH1 = ["สวัสดี","ใช่","ไม่","โปรด","ขอบคุณ","ลาก่อน","ขอโทษ","น้ำ","อาหาร","ช่วย"];
+const tableauImage =  []
+
+const imageHello = new Image();
+imageHello.src="Images/Carte/hello.jpg"
+tableauImage.push(imageHello);
+
+const imageYes = new Image();
+imageYes.src = "Images/Carte/Yes2.jpg"
+tableauImage.push(imageYes)
+
+const imageNo = new Image();
+imageNo.src= "Images/Carte/No.jpg"
+tableauImage.push(imageNo)
+
+const imagePlease = new Image();
+imagePlease.src ="Images/Carte/Please.jpg"
+tableauImage.push(imagePlease)
+
+const imageThank = new Image();
+imageThank.src = "Images/Carte/thank.jpg"
+tableauImage.push(imageThank)
+
+const ImageBye = new Image();
+ImageBye.src = "Images/Carte/Bye.jpg"
+tableauImage.push(ImageBye)
+
+const ImageSorry = new Image();
+ImageSorry.src = "Images/Carte/Sorry.jpg"
+tableauImage.push(ImageSorry)
+
+const ImageWater = new Image();
+ImageWater.src= "Images/Carte/Water.jpg"
+tableauImage.push(ImageWater)
+
+const ImageFood = new Image();
+ImageFood.src ="Images/Carte/Food.jpg"
+tableauImage.push(ImageFood)
+
+const ImageHelp = new Image();
+ImageHelp.src="Images/Carte/Help.jpg"
+tableauImage.push(ImageHelp)
---
+++ script/script.js
@@ -0,0 +1,99 @@
+
+
+
+
+
+
+
+
+
+    let boutonSuivant = document.getElementById("next")
+    let boutonPrecedent =document.getElementById("previous")
+    let English= document.getElementById("English")
+    let Thai= document.getElementById("Thai")
+    let carte= document.querySelector(".carte")
+    let i=0;
+  
+    
+    carte.src=tableauImage[i].src
+        English.innerText=tableauMotsEN1[i]
+        Thai.innerText=tableauMotsTH1[i]
+        boutonPrecedent.disabled=true;
+    let limit=tableauMotsEN1.length-1
+   
+      
+    
+    console.log(carte)
+   
+        
+        
+
+
+    function next (){
+       
+        
+        if(i===limit){
+            boutonSuivant.disabled=true;
+            English.innerText=tableauMotsEN1[i]
+            Thai.innerText=tableauMotsTH1[i]
+            carte.src=tableauImage[i].src
+            console.log(i)
+            
+        }
+        else{
+            boutonSuivant.disabled=false;
+            boutonPrecedent.disabled=false;
+            i++
+            English.innerText=tableauMotsEN1[i]
+        Thai.innerText=tableauMotsTH1[i]
+        carte.src=tableauImage[i].src
+        }
+        
+    }
+
+     
+        
+       
+       
+
+        
+     
+        
+        
+            
+            
+        
+    
+        
+  
+function precedent(){
+        if(i===0){
+            boutonPrecedent.disabled=true;
+            
+            
+        }
+        else{
+            boutonPrecedent.disabled=false;
+            
+            boutonSuivant.disabled=false;
+            i--
+            English.innerText=tableauMotsEN1[i]
+        Thai.innerText=tableauMotsTH1[i]
+        carte.src=tableauImage[i].src
+        console.log(i)
+        }
+    }
+
+       
+
+    // }
+// )
+
+function Menu(){
+    document.location.href="index.html"
+
+}
+
+function Learn(){
+    document.location.href="learn.html"
+}
