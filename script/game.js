093a1ddc862b55b52bc8ef5f3888d4b454088df9
Hopeful117 <135059935+Hopeful117@users.noreply.github.com>
2024-09-15 23:11:31 +0200

    update

--- script/game.js
+++ script/game.js
@@ -4,20 +4,39 @@ function rdImage(){
     const answer= document.getElementById("answer")
     const validate= document.getElementById("validate")
     i=randomIndex
-    console.log(i)
+    score=0;
+   
     const question=tableauImage[randomIndex]
     alea.src=question.src
-    console.log(question)
-    console.log(alea.src)
+    
 
     answer.hidden=false
     validate.hidden=false
+    scorezone.hidden=false
+    
+   
 
-    const reponse = document.querySelectorAll('#answer input [type="radio"]')
-    for(let j=0;j<reponse.length;j++){
+    const reponse = document.querySelectorAll('#answer label')
+   
+    const j =Math.floor(Math.random()*reponse.length);
+   
+    for (let k=0;k<reponse.length;k++){
+        let rdanswer=getrdanswer();
+        
+        reponse[k].innerHTML=tableauMotsTH1[rdanswer]
+       
 
 
     }
+
+
+    const bonneReponse=reponse[j];
+    
+    bonneReponse.innerHTML=tableauMotsTH1[i];
+    
+     
+
+    
    
     
     
@@ -29,3 +48,11 @@ function Menu(){
     document.location.href="index.html"
 
 }
+
+function getrdanswer(){
+    return Math.floor(Math.random()*tableauMotsTH1.length)
+
+}
+
+
+        
